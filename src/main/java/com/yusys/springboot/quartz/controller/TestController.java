f7f8e8ed295ef99506350d5ec58ea8b3fa7ba25b
huyang <1981115225@qq.com>
2019-10-16 19:31:00 +0800

    SpringBoot-Quartz_v1 2019/10/16

--- src/main/java/com/yusys/springboot/quartz/controller/TestController.java
+++ src/main/java/com/yusys/springboot/quartz/controller/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * Test
  * Created by huyang on 2019/10/16.
  */
 @RestController
